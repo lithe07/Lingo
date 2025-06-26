c8e150773ec59b016b45ef99786a865cc26519e6
lithe07 <99078068@mydavinci.nl>
2025-06-26 13:24:31 +0200

    data set done

---
+++ data.py
@@ -0,0 +1,34 @@
+# Dat.py
+
+# Teams en Beurt
+teams = ["Team 1", "Team 2"]
+beurt = 0
+
+# Namen opslaan
+team1 = []
+team2 = []
+
+# Ballenbak per team
+ballenBak1 = ["🟢", "🟢", "🟢", "🔴", "🔴", "🔴", 2, 4, 6, 8, 10, 12, 14, 16]
+ballenBak2 = ["🟢", "🟢", "🟢", "🔴", "🔴", "🔴", 1, 3, 5, 7, 9, 13, 15, 17]
+
+
+# Team data (per team)
+teamData = [
+    {"groen": 0, "rood": 0, "goedGeraden": 0, "foutOpRij": 0},
+    {"groen": 0, "rood": 0, "goedGeraden": 0, "foutOpRij": 0}
+]
+
+
+# Bingo-kaarten per team
+bingo_kaarten = [
+    [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12],
+     [13, 14, 15, 16]],
+
+    [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12],
+     [13, 14, 15, 16]]
+]
